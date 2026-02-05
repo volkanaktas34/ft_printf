57648e742e7699d8cb2de0e2e97e47448032c85b
volkanaktas34 <volkan.aktas@outlook.com.tr>
2026-02-05 13:51:36 +0300

    Header dosyasında düzeltme

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vaktas<vaktas@student.42istanbul.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 17:12:20 by vaktas            #+#    #+#             */
-/*   Updated: 2026/02/04 12:30:20 by vaktas           ###   ########.tr       */
+/*   Updated: 2026/02/05 13:51:15 by vaktas           ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 # include <stdarg.h>
 # include <unistd.h>
 
+int	ft_printf(const char *format, ...);
 int	ft_putchar(char c);
 int	ft_putstr(char *s);
 int	ft_putnbr(long n);
 int	ft_puthex(unsigned long n, char *base);
 int	ft_putptr(unsigned long n);
 int	ft_check_format(va_list args, char format);
-int	ft_printf(const char *format, ...);
 
 #endif
